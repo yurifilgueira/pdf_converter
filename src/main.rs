fde3fea679c3baf5067865eff084a483c0478280
Yuri Filgueira <yurimedeiros141@gmail.com>
2024-05-31 14:51:30 -0300

    Feat: write_file function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::{io, path::{self, PathBuf}};
-use native_dialog::{FileDialog, MessageDialog, MessageType};
 
+use std::{fs::{create_dir, File, OpenOptions}, io::{self, Error, Write}, path::PathBuf};
+use native_dialog::{FileDialog, MessageDialog, MessageType};
 enum FileType {
     Pdf,
     Txt
@@ -10,15 +10,20 @@ fn convert(file_type: FileType) {
 
     match file_type {
         FileType::Pdf => {
-            let path = open_dialog_box("pdf");
-            todo!();
+            
+            let path = open_file_dialog_box("pdf");
+
+            let bytes = read_file(&path);
+            let content = pdf_extract::extract_text_from_mem(&bytes.unwrap()).unwrap();
+            
+            let _ = write_file(&content);
+
         },
         FileType::Txt => {
-            let path = open_dialog_box("txt");
+            let _path = open_file_dialog_box("txt");
             todo!();
         }
     }
-
 }
 
 fn open_message_dialog(path: &PathBuf) -> bool {
@@ -30,7 +35,7 @@ fn open_message_dialog(path: &PathBuf) -> bool {
     .unwrap();
 }
 
-fn open_dialog_box(file_type: &str) -> Option<PathBuf> {
+fn open_file_dialog_box(file_type: &str) -> Option<PathBuf> {
 
     let path = FileDialog::new()
     .set_location("~/Downloads")
@@ -53,6 +58,94 @@ fn open_dialog_box(file_type: &str) -> Option<PathBuf> {
         return None;
     }
         
+}
+
+fn open_path_dialog_box() -> Option<PathBuf> {
+
+    let path = FileDialog::new()
+    .set_location("~/Documents/")
+    .show_open_single_dir()
+    .unwrap();
+
+    let path = match path {
+        Some(path) => path,
+        None => panic!("Error"),
+    };
+
+    let yes = open_message_dialog(&path);
+
+
+    if yes {
+        return Some(path);
+    }
+    else {
+        return None;
+    }
+        
+}
+
+fn read_file(path: &Option<PathBuf>) -> io::Result<Vec<u8>>{
+    match path {
+        Some(p) => std::fs::read(p),
+        None => Err(io::Error::new(io::ErrorKind::NotFound, "Path not provided")),
+    }
+}
+
+fn write_file(content: &String) -> Result<(), Error> {
+
+    let path = get_path().unwrap();
+
+    if !path.exists() {
+        
+        match create_dir(&path) {
+            Ok(_) => println!("Directory '{}' created successfully!", &path.display()), 
+            Err(e) => println!("Error creating directory '{}': {}", &path.display(), e),
+        };
+        
+        let result = create_file(&path);
+
+        match result {
+            Ok(mut file) => {
+                if let Err(e) = file.write(content.as_bytes()) {
+                    eprintln!("Erro ao escrever no arquivo pdf_to_txt.txt: {}", e);
+                } else {
+                    println!("Arquivo pdf_to_txt.txt sobrescrito com sucesso!");
+                }
+            }
+            Err(e) => {
+                eprintln!("Erro ao abrir o arquivo pdf_to_txt.txt: {}", e);
+            }
+        }
+
+    }
+    
+    Ok(())
+}
+
+fn create_file(path: &PathBuf) -> Result<File, Error>{
+
+    let file = File::create(path)?;
+
+    Ok(file)
+}
+
+fn get_path() -> Option<PathBuf> {
+    let choosed_path = open_path_dialog_box();
+    let file_location = unwrap_pathbuf_to_string(&choosed_path).unwrap();
+
+    let file_location = format!("{}/Pdf Converter", file_location);
+
+    return Option::from(PathBuf::from(&file_location));
+}
+
+fn unwrap_pathbuf_to_string(path: &Option<PathBuf>) -> Result<String, &'static str> {
+    match path {
+        Some(p) => p
+            .to_str()
+            .map(|s| s.to_owned())
+            .ok_or("Path is None"),
+        None => Err("Path is None"),
+    }
 }
 
 
